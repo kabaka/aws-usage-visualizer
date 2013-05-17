2e3888b05f0c028d90b47d6499d9213addc847bc
Kyle Jonhson <kyle@vacantminded.com>
2013-05-17 03:40:46 +0000

    upload: skip existing files
    Also, we are now using aws-sdk rather than aws-s3 gem

--- lib/upload.rb
+++ lib/upload.rb
@@ -1,17 +1,24 @@
-require 'aws/s3'
-include AWS::S3
+require 'aws'
+require 'digest/md5'
+
+include AWS
 
 load 'lib/config.rb'
 
 print 'Connecting to S3...'
 
-Base.establish_connection! $conf[:AWS]
+s3 = S3.new $conf[:AWS]
 
 puts ' Done'
 
 print 'Getting bucket info...'
 
-bucket = Bucket.find $conf[:bucket]
+bucket = s3.buckets[$conf[:bucket]]
+
+unless bucket.exists?
+  puts ' Failed'
+  abort "bucket #{$conf[:bucket]} does not exist"
+end
 
 puts ' Done'
 
@@ -20,21 +27,21 @@ puts 'Processing files...'
 Dir['output/**/*'].each do |local|
   next if File.directory? local
 
-  name = local.match(/^output(\/.+)$/)[1]
-  print "  - #{name}"
+  name = local.match(/^output\/(.+)$/)[1]
+  print "  - #{name}..."
 
-  S3Object.store name, open(local),
-    $conf[:bucket], :access => :public_read
+  digest = Digest::MD5.hexdigest File.read local
+ 
+  remote = bucket.objects[name]
+
+  if remote.exists? and remote.etag.delete('"') == digest
+    puts ' Skipped Existing File'
+    next
+  end
+
+  remote.write Pathname.new(local),
+    acl: :public_read
 
   puts ' Done'
 end
 
-__END__
-
-bucket.objects.each do |obj|
-  remote = obj.key
-
-  if File.exists? "output/#{remote}"
-    puts "Existing file found: #{remote}"
-  end
-end
